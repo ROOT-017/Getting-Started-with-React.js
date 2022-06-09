ec578a7ce833bf65081fc3c87bca223c48fe3765
NKWETACHA TERENCE <nkwetachaterence17@gmail.com>
2022-06-09 18:26:25 +0100

    Word to all dev add

--- main.js
+++ main.js
@@ -25,3 +25,4 @@ const root = ReactDOM.createRoot(domContainer);
 root.render(e(LikeButton));
 
 console.log("As A dev learn to use debugger instead of console to debogg your code")
+console.log("As A dev learn to use debugger instead of console to debogg your code")
